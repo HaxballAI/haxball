6a49872f3c5116cce5adf63f9e4525e8385e9205
Majakas <taavet.kalda@gmail.com>
2019-07-02 14:58:57 +0100

    Added the base class Entity from which all the objects in the gamesim are derived from

--- game_simulator/gamesim.py
+++ game_simulator/gamesim.py
@@ -1,25 +1,29 @@
 import playeraction
 
-class Player:
-    def __init__(self,i_position,i_velocity,i_acceleration):
-        self.pos = i_position
-        self.vel = i_velocity
-        self.acc = i_acceleration
+# Base class for any entity, stores position, velocity, acceleration.
+class Entity:
+    def __init__(self, initial_position, initial_velocity, initial_acceleration):
+        self.pos = initial_position
+        self.vel = initial_velocity
+        self.acc = initial_acceleration
+
+
+class Player(Entity):
+    def __init__(self, initial_position, initial_velocity = 0, initial_acceleration = 0):
+        Entity.__init__(self, initial_position, initial_velocity, initial_acceleration)
         #Action is weird, not nice. Maybe make into a class?
         self.current_action = 0
 
 
-class Ball:
-    def __init__(self,initial_position,initial_velocity):
-        self.pos = initial_position
-        self.vel = initial_velocity
+class Ball(Entity):
+    def __init__(self, initial_position, initial_velocity = 0, initial_acceleration = 0):
+        Entity.__init__(self, initial_position, initial_velocity, initial_acceleration)
 
 
+class GoalPost(Entity):
+    def __init__(self, initial_position, initial_velocity = 0, initial_acceleration = 0):
+        Entity.__init__(self, initial_position, initial_velocity, initial_acceleration)
 
-class GoalPost:
-    def __init__(self,initial_position,initial_velocity):
-        self.pos = initial_position
-        self.vel = initial_velocity
 
 class GameSim:
     def __init__(self,players,balls,goalposts):
