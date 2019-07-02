aafd87c5712123ee7c027e9965b42529a7066855
pnielsen2 <pnielsen2@outlook.com>
2019-07-02 15:24:31 +0100

    updated is moving dir again

--- game_simulator/playeraction.py
+++ game_simulator/playeraction.py
@@ -9,15 +9,15 @@ class Action:
         return self.kick
 
     def isMovingDir(self,direction):
-        if (self.dir[0,0]==0 and direction == none)
-            or (self.dir[0,0]==1 and direction == "r")
-            or (self.dir[0,0]==2 and direction in ["ur","ru"])
-            or (self.dir[0,0]==3 and direction == "u")
-            or (self.dir[0,0]==4 and direction in ["ul","lu"])
-            or (self.dir[0,0]==5 and direction == "l")
-            or (self.dir[0,0]==6 and direction in ["dl","ld"])
-            or (self.dir[0,0]==7 and direction == "d")
-            or (self.dir[0,0]==8 and direction in ["dr","rd"]):
+        if (self.dir[0, 0] == 0 and direction == "still")
+            or (self.dir[0, 0] == 1 and direction == "r")
+            or (self.dir[0, 0] == 2 and direction in ["ur","ru"])
+            or (self.dir[0, 0] == 3 and direction == "u")
+            or (self.dir[0, 0] == 4 and direction in ["ul","lu"])
+            or (self.dir[0, 0] == 5 and direction == "l")
+            or (self.dir[0, 0] == 6 and direction in ["dl","ld"])
+            or (self.dir[0, 0] == 7 and direction == "d")
+            or (self.dir[0, 0] == 8 and direction in ["dr","rd"]):
             return True
         else:
             return False
