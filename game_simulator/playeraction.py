1553b700bbac49e5e9aec6143dc52f55e7ec816e
pnielsen2 <pnielsen2@outlook.com>
2019-07-02 15:15:55 +0100

    updated isMovingDir

--- game_simulator/playeraction.py
+++ game_simulator/playeraction.py
@@ -5,10 +5,25 @@ class Action:
         self.kick = isKicking
         self.dir = direction
 
-    def isKicking(self):
+        def isKicking(self):
         return self.kick
 
     def isMovingDir(self,direction):
+        if (self.dir[0,0]==0 and direction == none)
+            or (self.dir[0,0]==1 and direction == "r")
+            or (self.dir[0,0]==2 and direction in ["ur","ru"])
+            or (self.dir[0,0]==3 and direction == "u")
+            or (self.dir[0,0]==4 and direction in ["ul","lu"])
+            or (self.dir[0,0]==5 and direction == "l")
+            or (self.dir[0,0]==6 and direction in ["dl","ld"])
+            or (self.dir[0,0]==7 and direction == "d")
+            or (self.dir[0,0]==8 and direction in ["dr","rd"]):
+            return True
+        else:
+            return False
+
+
+
         #Return true if directions is current one
         #Directions should be a string like "ul" for up-left for
         #ease of use
