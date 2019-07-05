fdf900f30775da5333fbf1e0c524f87bf8ab9f45
Majakas <taavet.kalda@gmail.com>
2019-07-05 12:08:20 +0100

    Skeleton code for the Data Handler

---
+++ data_handler/datahandler.py
@@ -0,0 +1,18 @@
+class DataHandler:
+    def __init__(self, saving_destination):
+        continue
+
+    def loadIntoBuffer(self, content):
+        continue
+
+    def dumpBuffer(self):
+        continue
+
+    def loadFileToBuffer(self):
+        continue
+
+    def getNextStateFromBuffer(self):
+        continue
+
+    def getBuffer(self):
+        continue
