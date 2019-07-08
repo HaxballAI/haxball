4334736e7fc5995988d0fff1bc3f742f68686d64
pnielsen2 <pnielsen2@outlook.com>
2019-07-08 17:15:45 +0100

    don't fuckin know

--- moveclassifier.py
+++ moveclassifier.py
@@ -45,22 +45,14 @@ print(data)
 
 class TwoLayerNet(torch.nn.Module):
     def __init__(self, D_in, H, D_out):
-        """
-        In the constructor we instantiate two nn.Linear modules and assign them as
-        member variables.
-        """
+
         super(TwoLayerNet, self).__init__()
         self.linear1 = torch.nn.Linear(D_in, H)
         self.linear2 = torch.nn.Linear(H, D_out)
 
     def forward(self, x):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Tensors.
-        """
         h_relu = self.linear1(x).clamp(min=0)
-        y_pred = F.softmax(self.linear2(h_relu),dim=0)
+        y_pred = torch.cat([F.softmax(self.linear2(h_relu)[:9],dim=0),torch.sigmoid(self.linear2(h_relu)[-1])], dim=0)
         return y_pred
 
 
@@ -85,7 +77,7 @@ for t in range(1):
         y_pred = model(data[0][i])
 
         # Compute and print loss
-        loss = criterion(y_pred, data[1][i])
+        loss = criterion(y_pred, data[1])
         print(t, loss.item())
 
         # Zero gradients, perform a backward pass, and update the weights.
