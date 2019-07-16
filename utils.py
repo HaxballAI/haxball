20f063ed691674960e49fff12496273417687f19
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2019-07-16 14:17:01 +0100

    Remove random space

--- utils.py
+++ utils.py
@@ -5,7 +5,6 @@ def flatten(S):
         return S
     if isinstance(S[0], list):
         return flatten(S[0]) + flatten(S[1:])
-    if isinstance(S[0], type(np.array([])) ):
+    if isinstance(S[0], type(np.array([]))):
         return flatten(S[0].tolist()) + flatten(S[1:])
     return S[:1] + flatten(S[1:])
-
