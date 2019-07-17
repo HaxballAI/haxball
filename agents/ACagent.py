7727ef8f12849ea936f46ae7fd9dcb99faab4ce6
Sebastian Towers <sebastian@sebastian-debian.sjc.ox.ac.uk>
2019-07-17 15:36:01 +0100

    Changed comments to be appropriate

--- agents/ACagent.py
+++ agents/ACagent.py
@@ -3,13 +3,11 @@ from move_displayer import movedisplayer
 from random import randrange
 
 class ACAgent():
-    # A really clever agent that only returns random commands
+    # Agent that works off of a actor-critic model
     def __init__(self, network):
         self.network = network
 
-    def getRawAction(self, state, give_debug_surf = False):
-        # Returns raw action of the agent based on the key presses queried from
-        # the gui. Returns (dir_idx, kicking_state)
+    def getRawAction(self, state, give_debug_surf = False)
         movepred, kickpred , _ = model(state)
         ran_move = np.random.choice(len(movepred), p = torch.nn.Softmax(dim = 0)(movepred).detach().numpy())
         p_kick = float(kickpred[0])
@@ -21,4 +19,4 @@ class ACAgent():
             return (ran_move, ran_kick)
 
     def getAction(self, state = []):
-        return playeraction.Action(*self.network.getRawAction(state))
+        raise NotImplementedError
