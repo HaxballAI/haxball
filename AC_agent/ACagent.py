a2faf9b533c5d04336fc13dceb3adb574088d033
Majakas <taavet.kalda@gmail.com>
2019-07-17 15:06:45 +0100

    Added an ACagent with skeleton coded

--- AC_agent/ACagent.py
+++ AC_agent/ACagent.py
@@ -2,15 +2,16 @@ from game_simulator import playeraction
 
 from random import randrange
 
-class RetardedAgent():
+class ACAgent():
     # A really clever agent that only returns random commands
-    def __init__(self):
+    def __init__(self, network):
+        self.network = network
         pass
 
     def getRawAction(self, state = []):
         # Returns raw action of the agent based on the key presses queried from
         # the gui. Returns (dir_idx, kicking_state)
-        return randrange(9), randrange(2)
+        return self.network.getRawAction(state)
 
     def getAction(self, state = []):
-        return playeraction.Action(*self.getRawAction(state))
+        return playeraction.Action(*self.network.getRawAction(state))
