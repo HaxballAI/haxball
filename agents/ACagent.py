030ef840436cdbfca45ff75003d1c829a40f5941
Sebastian Towers <sebastian@sebastian-debian.sjc.ox.ac.uk>
2019-07-17 15:33:14 +0100

    Implemented ACAgent

--- agents/ACagent.py
+++ agents/ACagent.py
@@ -1,5 +1,5 @@
 from game_simulator import playeraction
-
+from move_displayer import movedisplayer
 from random import randrange
 
 class ACAgent():
@@ -7,10 +7,18 @@ class ACAgent():
     def __init__(self, network):
         self.network = network
 
-    def getRawAction(self, state = []):
+    def getRawAction(self, state, give_debug_surf = False):
         # Returns raw action of the agent based on the key presses queried from
         # the gui. Returns (dir_idx, kicking_state)
-        return self.network.getRawAction(state)
+        movepred, kickpred , _ = model(state)
+        ran_move = np.random.choice(len(movepred), p = torch.nn.Softmax(dim = 0)(movepred).detach().numpy())
+        p_kick = float(kickpred[0])
+        ran_kick = np.random.choice([False, True], p = [1 - p_kick, p_kick])
+        if give_debug_surf:
+            debug_surf = movedisplayer.drawMove(torch.nn.Softmax(dim = 0)(movepred).detach().numpy(), ran_move)
+            return (ran_move, ran_kick), debug_surf
+        else:
+            return (ran_move, ran_kick)
 
     def getAction(self, state = []):
         return playeraction.Action(*self.network.getRawAction(state))
