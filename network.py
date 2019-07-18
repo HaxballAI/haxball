3269b37b9b57f600978a279b45fc290452ce86ba
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2019-07-18 21:33:24 +0100

    Use D_out in Policy

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ class Policy(torch.nn.Module):
     def __init__(self, D_in = 12, D_hid = 50, D_out = 10):
         super(Policy, self).__init__()
         self.affine1 = torch.nn.Linear(D_in, D_hid)
-        self.move_head = torch.nn.Linear(D_hid, 9)
+        self.move_head = torch.nn.Linear(D_hid, D_out - 1)
         self.kick_head = torch.nn.Linear(D_hid, 1)
         self.value_head = torch.nn.Linear(D_hid, 1)
 
