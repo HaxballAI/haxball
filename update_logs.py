a6acdb6fba0cd1406e015dba32780a74c69c6a88
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2019-07-20 13:06:47 +0100

    Fix update_logs.py

--- update_logs.py
+++ update_logs.py
@@ -5,9 +5,9 @@ from game_simulator import playeraction
 import sys
 
 def convertBall(b): return b
-def convertPlayer(p): return log.PlayerState(playeraction.Action(p.move, p.kick))
-def convertFrame(f): return log.Frame(map(convertPlayer, f.blues), map(convertPlayer, f.reds), map(convertBall, f.balls))
-def convertGame(g): return log.Game(g.red_goals, g.blue_goals, map(convertFrame, g.frames))
+def convertPlayer(p): return log.PlayerState(p.x, p.y, p.vx, p.vy, playeraction.Action(p.move, p.kick))
+def convertFrame(f): return log.Frame([convertPlayer(p) for p in f.blues], [convertPlayer(p) for p in f.reds], [convertBall(b) for b in f.balls])
+def convertGame(g): return log.Game(g.red_goals, g.blue_goals, [convertFrame(f) for f in g.frames])
 
 for f in sys.argv[1:]:
     print(f"Converting {f}")
