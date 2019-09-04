a4bf1724ec75ee62b40389258bcf58516172781a
Sebastian <towers.towers.sebastian@gmail.com>
2019-09-05 00:44:41 +0100

    Update ppo_runner.py
    added file for running ppo algorithm

--- ppo_runner.py
+++ ppo_runner.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     mod2 = torch.load("models/seb_mod_baseline.model")
     mod1.to("cuda")
     mod2.to("cpu")
-    trainer = ppo.PPOTrainer(mod1, solo.env_constructor(mod2), 10, parallelise = False)
+    trainer = ppo.PPOTrainer(mod1, solo.env_constructor(mod2), 10, parallelise = True)
     trainer.train(100)
     torch.save(mod1, "models/seb_test.model")
